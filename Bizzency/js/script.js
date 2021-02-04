9d5374efea69f424f3731112bfe35e764438b4cf
Seam Rahman <78542433+seamrahman1@users.noreply.github.com>
2021-02-04 18:39:22 +0600

    Add files via upload

---
+++ Bizzency/js/script.js
@@ -0,0 +1,25 @@
+$('#jq').LineProgressbar({
+percentage:90,
+radius: '3px',
+height: '10px',
+fillBackgroundColor: 'rgb(42, 72, 150)'
+});
+$('#html').LineProgressbar({
+percentage:85,
+radius: '3px',
+height: '10px',
+fillBackgroundColor: '#5d3b15'
+});
+$('#css').LineProgressbar({
+percentage:75,
+radius: '3px',
+height: '10px',
+fillBackgroundColor: 'rgb(52, 74, 31)'
+});
+
+$('#ps').LineProgressbar({
+percentage:80,
+radius: '3px',
+height: '10px',
+fillBackgroundColor: 'rgb(57, 41, 96)'
+});
